8340b5ba5f417b34c7a1d86909c5741f6a49140f
PisethC <chuonpiseth05@gmail.com>
2025-10-15 15:57:36 +0700

    Updated

--- models/hostel_student.py
+++ models/hostel_student.py
@@ -22,21 +22,22 @@ class HostelStudent(models.Model):
 
     name = fields.Char("Student Name")
     gender = fields.Selection([("male", "Male"),
-        ("female", "Female"), ("other", "Other")],
-        string="Gender", help="Student gender")
+                               ("female", "Female"), ("other", "Other")],
+                              string="Gender", help="Student gender")
     active = fields.Boolean("Active", default=True,
-        help="Activate/Deactivate hostel record")
+                            help="Activate/Deactivate hostel record")
     room_id = fields.Many2one("hostel.room", "Room",
-        help="Select hostel room")
+                              help="Select hostel room")
+    hostel_id = fields.Many2one("hostel.hostel", related="room_id.hostel_id")
     status = fields.Selection([("draft", "Draft"),
-        ("reservation", "Reservation"), ("pending", "Pending"),
-        ("paid", "Done"),("discharge", "Discharge"), ("cancel", "Cancel")],
-        string="Status", copy=False, default="draft",
-        help="State of the student hostel")
+                               ("reservation", "Reservation"), ("pending", "Pending"),
+                               ("paid", "Done"), ("discharge", "Discharge"), ("cancel", "Cancel")],
+                              string="Status", copy=False, default="draft",
+                              help="State of the student hostel")
     admission_date = fields.Date("Admission Date",
-        help="Date of admission in hostel",
-        default=fields.Datetime.today)
+                                 help="Date of admission in hostel",
+                                 default=fields.Datetime.today)
     discharge_date = fields.Date("Discharge Date",
-        help="Date on which student discharge")
+                                 help="Date on which student discharge")
     duration = fields.Integer("Duration", compute="_compute_check_duration", inverse="_inverse_duration",
-                               help="Enter duration of living")
+                              help="Enter duration of living")
